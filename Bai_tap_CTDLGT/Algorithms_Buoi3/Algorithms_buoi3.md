86e545be298fc180415abacadafebdb0632fe3c6
anhtuan5296 <khukhoam1987@gmail.com>
2023-07-10 22:31:21 +0700

    Day_04

--- Bai_tap_CTDLGT/Algorithms_Buoi3/Algorithms_buoi3.md
+++
@@ -1,7 +0,0 @@
-## Mandatory:   
-[414. Third Maximum Number](https://leetcode.com/problems/third-maximum-number/submissions/967474868/)  
-[1051. Height Checker](https://leetcode.com/problems/height-checker/submissions/967787755/)  
-[2418. Sort the People](https://leetcode.com/problems/sort-the-people/submissions/967509491/)  
-[1636. Sort Array by Increasing Frequency](https://leetcode.com/problems/sort-array-by-increasing-frequency/submissions/967604408/)  
-## Optional:
-[389. Find the Difference ](https://leetcode.com/problems/find-the-difference/submissions/967609551/)
---
+++ main.js
@@ -0,0 +1,72 @@
+// Mảng màu
+let colors = [
+  '#3498db',
+  '#9b59b6',
+  '#e74c3c',
+  '#2c3e50',
+  '#d35400',
+];
+
+// Số lượng box ban đầu
+let totalBox = 5;
+
+// Lấy các phần tử DOM cần sử dụng
+const boxesContainer = document.querySelector('.boxes');
+const scoreElement = document.querySelector('.points');
+const moreBoxButton = document.getElementById('btn');
+
+// Render box ban đầu
+renderBoxes(totalBox);
+
+// Đăng ký click cho các box
+boxesContainer.addEventListener('click', function (event) {
+  const clickedBox = event.target;
+  if (clickedBox.classList.contains('box')) {
+    removeBox(clickedBox);
+  }
+});
+
+// Đăng ký click cho nút "more box"
+moreBoxButton.addEventListener('click', function () {
+  addMoreBoxes(5);
+});
+
+// Hàm render các box ban đầu
+function renderBoxes(count) {
+  for (let i = 0; i < count; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.backgroundColor = colors[i % colors.length];
+    boxesContainer.appendChild(box);
+  }
+  updateTotalBox();
+}
+
+// Hàm thêm box mới
+function addBox() {
+  const box = document.createElement('div');
+  box.classList.add('box');
+  box.style.backgroundColor = colors[(totalBox - 1) % colors.length];
+  boxesContainer.appendChild(box);
+  updateTotalBox();
+}
+
+// Hàm xóa box
+function removeBox(box) {
+  box.remove();
+  updateTotalBox();
+}
+
+// Hàm cập nhật số lượng total box
+function updateTotalBox() {
+  const boxElements = document.querySelectorAll('.box');
+  totalBox = boxElements.length;
+  scoreElement.textContent = totalBox;
+}
+
+// Hàm thêm nhiều box
+function addMoreBoxes(count) {
+  for (let i = 0; i < count; i++) {
+    addBox();
+  }
+}
